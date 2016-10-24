fa3eed9e4684c3fbb55da3d7a1903e58155fe590
Scott Weiss <scott.weiss@emc.com>
2016-10-24 13:00:19 -0400

    initial commit
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'layerx-chef'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures layerx-chef'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
